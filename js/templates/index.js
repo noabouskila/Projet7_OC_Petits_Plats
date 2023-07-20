146304a95e81a5394eb6e3ba2adf9ebddadfb81e
John Doe <noabouskil@gmail.com>
2023-07-20 15:10:30 +0200

    filtre principal fait sans filtrage des filtres avancés

--- js/templates/index.js
+++ js/templates/index.js
@@ -18,9 +18,13 @@ heads.forEach((head) => {
 const countRecipes = document.getElementById('countRecipes')
 let totalRecipes = 0
 
-// INSERTION INITIALE DES RECETTES
-// console.log(recipes)
 
+// INSERTION INITIALE DES RECETTES
+// const sectionCards = document.getElementById('sectionCards')
+// const sectionCard = document.createElement("div")
+// sectionCard.classList.add("sectionCard")
+
+// console.log(recipes)
 function createRecipeCards(recipes) {
     recipes.forEach(recipe => {
       createRecipeCard(recipe);
@@ -31,6 +35,7 @@ createRecipeCards(recipes);
 
 
 
+// generer les cards
 function createRecipeCard(recipe){
 
     const sectionCards = document.getElementById('sectionCards')
@@ -107,6 +112,77 @@ function closeBtn() {
     tagDiv.style.display = "none";
 }
   
+
+
+
+// Moteur de recherche principal
+const mainSearchEngine = document.getElementById("mainSearchEngine")
+mainSearchEngine.addEventListener("change",searchRecipe)
+
+// fonction de recherche moteur de recherche principale
+function searchRecipe(e){
+    
+    // mot clé tapé par le user
+    let searchTerm = e.target.value.trim().replace(/\s+/g, ' ').toLowerCase()
+    console.log(searchTerm)
+
+    // verification  motclé > de 3 caracteres et  motclé valide
+    if(searchTerm.length !== '' && searchTerm.length >= 3 && validateEntry(searchTerm)){
+        console.log("+ que 3 caracteres")
+
+        // FONCTION FILTER : filterRecipe
+        const filterRecipe = recipes.filter(recipe =>{
+           const filterName = recipe.name.toLowerCase()
+           const filterDescription = recipe.description.toLowerCase()
+           const filterIngredient = recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase());
+           
+           return filterName.includes(searchTerm) || filterDescription.includes(searchTerm) || filterIngredient.includes(searchTerm);
+        })
+        console.log(filterRecipe)
+
+        // affichage du contenu ET du nombre total de recettes
+        if (filterRecipe.length === 0) {
+            sectionCards.innerHTML = `Aucune recette ne contient '${searchTerm}'avec les éléments selctionnés. Vous pouvez chercher "tarte aux pommes", "poisson", etc.`;
+            countRecipes.innerHTML = `${filterRecipe.length} recettes`;
+        }
+        else{
+            updateRecipeDisplay(filterRecipe);
+        }
+        // let t1 = performance.now();
+        // console.log("L'appel a demandé " + (t1-t0)+ " ms.")
+
+    }
+    else{
+        console.log("- que 3 caracteres")
+        resetRecipeDisplay();
+    }
+
+}
+
+// Fonction pour l'affichage des recettes filtrées
+function updateRecipeDisplay(filterRecipe) {
+    sectionCards.innerHTML = "";
+    createRecipeCards(filterRecipe);
+    countRecipes.innerHTML = `${filterRecipe.length} recettes`;
+}
+
+// Fonction pour réinitialiser l'affichage des recettes
+function resetRecipeDisplay() {
+    sectionCards.innerHTML = "";
+    createRecipeCards(recipes);
+    countRecipes.innerHTML = `${recipes.length} recettes`;
+}
+
+//Fonction pour verifier les caractères non autorisés
+function validateEntry(str) {
+    var letters =
+      /[^a-zA-Z'áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ ]+/;
+    if (str.match(letters)) {
+      return false;
+    }
+    return true;
+}
+
 
 
 
